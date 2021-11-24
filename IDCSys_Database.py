4225873d0cba88a44580b67744ec6a7866704819
Fraxinus001 <63859985+Fraxinus001@users.noreply.github.com>
2021-11-24 13:35:43 +0800

    Add files via upload

--- IDCSys_Database.py
+++ IDCSys_Database.py
@@ -1,28 +1,49 @@
+
 import pickle
 import datetime
 import base64
+import AES256_synth
 from collections import defaultdict
 
 
 class IDCSys_Database:
     def __init__(self):
         DateTimeNow = datetime.datetime.today().strftime("%B %d, %Y - %I:%M:%S%p")
-        d1 = self.read("dat001.db")
-        d2 = self.read("dat002.db")
-        d3 = self.read("dat003.db")
-        d4 = self.read("dat004.db")
-        dat001 = pickle.load(d1)
-        dat002 = pickle.load(d2)
-        dat003 = pickle.load(d3)
-        dat004 = pickle.load(d4)
+        #dat001 = self.readmode("dat001.db")
+        #dat002 = self.readmode("dat002.db")
+        #dat003 = self.readmode("dat003.db")
+        #dat004 = self.readmode("dat004.db")
+        #d1 = pickle.load(dat001)
+        #d2 = pickle.load(dat002)
+        #d3 = pickle.load(dat003)
+        #d4 = pickle.load(dat004)
     
-    def read(self, data):
+    def readmode(self, data):
         return open(data, "rb")
     
-    def write(self, data):
+    def writemode(self, data):
         return open(data, "wb")
     
-class IDCSys_Database_Reset(IDCSys_Database):
+    def w_pop_(self, dx, UUID, input1):
+        dx[UUID] = dx.pop(input1)
+        
+    def w_change_001(self, dx, UUID, A, index1, B):
+        a = dx[UUID].index(A)
+        b = dx[UUID][a]
+        dx[UUID][index1].pop(B)
+        return
+    
+    def w_update(self, f, dx):
+        dx.update(f)
+        
+    def w_dump(self, dx, data):
+        pickle.dump(dx, data)
+        
+    def f_close(self, dx):
+        dx.close()
+        
+    
+class IDCSys_Database_Test(IDCSys_Database):
     def __init__(self):
         self.d1a = {b'rWOmY9hR0FwFRVrlRYKEyjH4jq+Yj6ZKlzCNKNwRnsxxFKQrjojVYb7QjtQyuIz+': [b'Raf', b'Dec. 8, 2020']}
         usr1 = b'rWOmY9hR0FwFRVrlRYKEyjH4jq+Yj6ZKlzCNKNwRnsxxFKQrjojVYb7QjtQyuIz+'
@@ -41,4 +62,25 @@ class IDCSys_Database_Reset(IDCSys_Database):
     def dump(self):
         self.d1a
         
-IDCSys_Database_Reset()
+        
+class IDCSys_Database_Reset(IDCSys_Database):
+    def __init__(self):
+        self.DateTimeNow = datetime.datetime.today().strftime("%B %d, %Y - %I:%M:%S%p")
+        
+    def gen_UUID(self):
+        self.UUID = AES256_synth.KeyEncrypt(self.DateTimeNow).KeyEncrypt()
+        return self.UUID
+    
+    def maintemplate(self):
+        UUID = self.gen_UUID()
+        return {UUID:[]}
+    
+    def execwrite(self):
+        self.maintemplate = self.maintemplate()
+        self.writemode = self.writemode("dat001.db")
+        self.w_dump(self.maintemplate, self.writemode)
+        print(self.maintemplate)
+        a = pickle.load(self.readmode("dat001.db"))
+        print(f"debug: {a}")
+
+print(IDCSys_Database_Reset().execwrite())
