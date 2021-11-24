e6ee0aa2da1b6452f7bb3743dda1907659f26f61
Fraxinus001 <63859985+Fraxinus001@users.noreply.github.com>
2021-11-24 12:20:59 +0800

    Add files via upload

--- IDCSys_Database.py
+++ IDCSys_Database.py
@@ -1,10 +1,11 @@
 
 import pickle
 import datetime
+import base64
 from collections import defaultdict
 
 
-class IDCSys_Database():
+class IDCSys_Database:
     def __init__(self):
         DateTimeNow = datetime.datetime.today().strftime("%B %d, %Y - %I:%M:%S%p")
         d1 = self.read("dat001.db")
@@ -23,4 +24,22 @@ class IDCSys_Database():
         return open(data, "wb")
     
 class IDCSys_Database_Reset(IDCSys_Database):
-    def dump
+    def __init__(self):
+        self.d1a = {b'rWOmY9hR0FwFRVrlRYKEyjH4jq+Yj6ZKlzCNKNwRnsxxFKQrjojVYb7QjtQyuIz+': [b'Raf', b'Dec. 8, 2020']}
+        usr1 = b'rWOmY9hR0FwFRVrlRYKEyjH4jq+Yj6ZKlzCNKNwRnsxxFKQrjojVYb7QjtQyuIz+'
+        print(f"debug: {self.d1a[usr1]}")
+        print(f"debug: {self.d1a[usr1][0]}")
+        self.d1a[usr1][0] = base64.b64encode(self.d1a[usr1][0])
+        self.d1a[usr1][1] = base64.b64encode(self.d1a[usr1][1])
+        print(f"debug: {self.d1a}")
+        self.d1a[usr1][0] = base64.b64decode(self.d1a[usr1][0])
+        self.d1a[usr1][1] = base64.b64decode(self.d1a[usr1][1])
+        print(f"debug: {self.d1a}")
+        self.d1a[usr1][0] = self.d1a[usr1][0].decode()
+        self.d1a[usr1][1] = self.d1a[usr1][1].decode()
+        print(f"debug: {self.d1a}")
+
+    def dump(self):
+        self.d1a
+        
+IDCSys_Database_Reset()
